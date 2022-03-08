e8d91054446186da775d08419670ac58c4bcde90
Manan Kanani <kanani.manan5@gmail.com>
2022-03-08 10:45:38 +0530

    website upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Manan Kanani - Portfolio'"; //letter subject
+	$to = 'kanani.manan5@gmail.com'; // Enter Your E-mail
+	$from = 'kanani.manan5@gmail.com'; // Admin e-mail
+	$fromName = 'Manan Kanani - Porfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
